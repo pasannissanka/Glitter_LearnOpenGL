60e71933474f5b5308925930f6ad30368214e539
pasan nissanka <pasannissanka@hotmail.com>
2021-01-11 15:00:58 +0530

    /Lighting/Light-casters - Point lights

--- Glitter/Shaders/fragment_shader_lighting.glsl
+++ Glitter/Shaders/fragment_shader_lighting.glsl
@@ -8,12 +8,16 @@ struct Material {
 
 
 struct Light {
-    // vec3 position; // no longer necessary when using directional lights.
-    vec3 direction;
+    vec3 position; // no longer necessary when using directional lights.
+    // vec3 direction;
 
     vec3 ambient;
     vec3 diffuse;
     vec3 specular;
+
+    float constant;
+    float linear;
+    float quadratic;
 };
 
 out vec4 FragColor;
@@ -29,12 +33,14 @@ uniform vec3        viewPos;
 
 void main()
 {
+    float distance = length(light.position - FragPos);
+    float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
     // ambient
     vec3 ambient = light.ambient * texture(material.diffuse, TexCoords).rgb;
   	
     // diffuse 
     vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(-light.direction);
+    vec3 lightDir = normalize(light.position - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = light.diffuse * diff * texture(material.diffuse, TexCoords).rgb;  
     
@@ -42,7 +48,11 @@ void main()
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);  
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-    vec3 specular = light.specular * spec * texture(material.specular, TexCoords).rgb;  
+    vec3 specular = light.specular * spec * texture(material.specular, TexCoords).rgb;
+
+    ambient *= attenuation;
+    diffuse *= attenuation;
+    specular *= attenuation;
         
     vec3 result = ambient + diffuse + specular;
     FragColor = vec4(result, 1.0);
--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -226,12 +226,17 @@ int main(int argc, char* argv[])
         // use Shader
         lightingShader.Use();
         lightingShader.setVec3("viewPos", camera.Position);
-        lightingShader.setVec3("light.direction", -0.2f, -1.0f, -0.3f);
+        // lightingShader.setVec3("light.direction", -0.2f, -1.0f, -0.3f);
+        lightingShader.setVec3("light.position", lightPos);
         
         // light properties
         lightingShader.setVec3("light.ambient", 0.2f, 0.2f, 0.2f);
         lightingShader.setVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
         lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
+
+        lightingShader.setFloat("light.constant", 1.0f);
+        lightingShader.setFloat("light.linear", 0.09f);
+        lightingShader.setFloat("light.quadratic", 0.032f);
 
         // material properties
         lightingShader.setFloat("material.shininess", 64.0f);
